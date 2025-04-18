538267d5b7988ca63f628e2080804b1e1e92f948
karansaini <karan.saj@gmail.com>
2025-04-18 09:53:42 +0530

    Correct response status being returned with the api response

--- src/main/java/com/bank/userManagement/controller/UserController.java
+++ src/main/java/com/bank/userManagement/controller/UserController.java
@@ -2,10 +2,17 @@ package com.bank.userManagement.controller;
 
 import com.bank.userManagement.dto.UserDTO;
 import com.bank.userManagement.services.UserService;
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/user")
+@Validated
+@Slf4j
 public class UserController {
 
     final private UserService userService;
@@ -15,13 +22,13 @@ public class UserController {
     }
 
     @GetMapping("/{id}")
-    public UserDTO fetchUserDetails(@PathVariable Long id) {
-        return userService.fetchUserDetails(id);
+    public ResponseEntity<UserDTO> fetchUserDetails(@PathVariable Long id) {
+        return new ResponseEntity<>(userService.fetchUserDetails(id), HttpStatus.OK);
     }
 
     @PostMapping()
-    public UserDTO createUser(@RequestBody UserDTO userDetails) {
-        return userService.createUser(userDetails);
+    public ResponseEntity<UserDTO> createUser(@Valid @RequestBody UserDTO userDetails) {
+        return new ResponseEntity<>(userService.createUser(userDetails), HttpStatus.CREATED);
     }
 
 
